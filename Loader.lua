c3b1a388e8b9c466a4c50c7f452efdca58b3a577
KingFreeAllHub <94217999+KingFreeAllHub@users.noreply.github.com>
2021-11-14 21:08:20 +0700

    1

--- Loader.lua
+++
@@ -1,7 +0,0 @@
-local key = _G.Key
-local check = "https://raw.githubusercontent.com/KingFreeAllHub/King-Hub-Blox-Fruit/main/check.php?key=" .. key
-if game:HttpGet(check) == "Whitelisted" then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/KingFreeAllHub/King-Hub-Blox-Fruit/main/script.lua"))()
-else
-game.Players.LocalPlayer:Kick("เสือกควยไรไอเหี้ยkeyไม่ถูกนะ")
-end
